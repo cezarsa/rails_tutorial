5506a3e4e6cd04a6a9ca28837d62c37d8a8a3d94
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2013-10-28 23:38:58 -0200

    cores para shell vagrant

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -7,6 +7,8 @@ source ~/.rvm/scripts/rvm
 rvm use --install 2.0.0
 rvm use 2.0.0@rails_tutorial --create
 
+echo "source ~/.profile" >> ~/.bash_profile
+
 gem install rails --no-ri --no-rdoc
 
 sudo apt-get install git -y
