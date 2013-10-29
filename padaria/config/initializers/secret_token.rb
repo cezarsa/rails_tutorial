179497924558daa79643e906423e2e6d64a5ed67
vagrant <vagrant@vagrant-precise64.(none)>
2013-10-29 00:06:45 -0200

    Commit inicial

---
+++ padaria/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Padaria::Application.config.secret_key_base = 'a01205a38b4c4366fe4d7851ff483bc64c4db41ea1ec6cce3c5276e0afc70d5170f8634dfc383a5eee300e248556f65ba9a6381c6b170ecbcc1ed3f4df44a8d3'
