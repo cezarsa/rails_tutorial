ef1a8628129fb50fa91e121f121b7a40b2866cfe
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2013-10-31 14:39:04 -0200

    fix provision

--- provision.sh
+++ provision.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
 curl -L https://get.rvm.io | bash -s stable
-
 source ~/.rvm/scripts/rvm
 
 rvm use --install 2.0.0
 rvm use 2.0.0@rails_tutorial --create
 
-[[ `id -un` == 'vagrant' ]] && echo "source ~/.profile" >> ~/.bash_profile
-
 gem install rails --no-ri --no-rdoc
 
-[[ `which apt-get` ]] && sudo apt-get update && sudo apt-get install git nodejs -y
-[[ `which brew` ]] && brew install git nodejs
+[[ `id -un` == 'vagrant' ]] && echo "source ~/.profile" >> ~/.bash_profile
+[[ `which apt-get` ]] && [[ `id -un` == 'vagrant' ]] && sudo apt-get update && sudo apt-get install git nodejs -y
+[[ `which brew` ]] && brew install nodejs
 
 
