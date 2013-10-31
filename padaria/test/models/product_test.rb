2a7a3bc6177af7da5e0221a38f42e105cca20c96
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2013-10-31 13:29:33 -0200

    product validations

--- padaria/test/models/product_test.rb
+++ padaria/test/models/product_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid product" do
+    p = Product.new(name: 'test', price: 1)
+    assert p.save
+  end
+
+  test "invalid empty product" do
+    p = Product.new
+    assert !p.save
+  end
+
+  test "invalid product without name" do
+    p = Product.new(price: 1)
+    assert !p.save
+  end
+
+  test "invalid product without price" do
+    p = Product.new(name: 'abc')
+    assert !p.save
+  end
+
+  test "invalid product with empty name" do
+    p = Product.new(name: '', price: 1)
+    assert !p.save
+  end
+
+  test "invalid product with price 0" do
+    p = Product.new(name: 'abc', price: 0)
+    assert !p.save
+  end
 end
