8787cac02066eb0b2ea2af3a3460ee0944dcfc50
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2013-10-31 02:04:32 -0200

    product new

--- padaria/test/controllers/products_controller_test.rb
+++ padaria/test/controllers/products_controller_test.rb
@@ -7,4 +7,12 @@ class ProductsControllerTest < ActionController::TestCase
     assert_select 'a[href*="welcome"]'
     assert_select 'a[href*="products/new"]'
   end
+
+  test "should get new" do
+    get :new
+    assert_not_nil assigns[:product]
+    assert_select 'form[action*="products"]'
+    assert_select 'input[type="text"]', count: 2
+    assert_select 'input[type="submit"]'
+  end
 end
