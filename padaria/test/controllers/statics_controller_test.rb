be6b1992c0a88ff122eb5b72969ef54c8f81473a
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2013-10-30 22:37:55 -0200

    Testando welcome

--- padaria/test/controllers/statics_controller_test.rb
+++ padaria/test/controllers/statics_controller_test.rb
@@ -4,6 +4,7 @@ class StaticsControllerTest < ActionController::TestCase
   test "should get welcome" do
     get :welcome
     assert_response :success
+    assert_select 'h1', /padaria/i
+    assert_select 'p', /bem-vindo/i
   end
-
 end
