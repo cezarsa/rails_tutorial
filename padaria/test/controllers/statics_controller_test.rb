c5e8e1655f6be01f79843ce471024f66aa43e69e
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2013-10-31 00:14:38 -0200

    Links para páginas

--- padaria/test/controllers/statics_controller_test.rb
+++ padaria/test/controllers/statics_controller_test.rb
@@ -6,11 +6,15 @@ class StaticsControllerTest < ActionController::TestCase
     assert_response :success
     assert_select 'h1', /padaria/i
     assert_select 'p', /bem-vindo/i
+    links = assert_select 'a'
+    assert_match /about/, links[0]['href']
   end
 
   test "should get about" do
     get :about
     assert_response :success
     assert_select '*', /fresquinho/
+    links = assert_select 'a'
+    assert_match /welcome/, links[0]['href']
   end
 end
